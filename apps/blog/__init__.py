71f9d5d1b8af8a6d64d5fb09466474c38d42762b
Bean-jun <1342104001@qq.com>
2021-08-14 00:28:22 +0800

    账号注册及登录

--- apps/blog/__init__.py
+++ apps/blog/__init__.py
@@ -4,8 +4,10 @@ from flask import Blueprint
 home_blueprint = Blueprint('blog', __name__)
 
 # 导入视图函数
-from . import home, template
+from . import home, template, account
 
 # 首页视图
 home_blueprint.add_url_rule('/', view_func=home.IndexView.as_view(name='index'))
 home_blueprint.add_url_rule('/detail/<int:id>/', view_func=home.DetailView.as_view(name='detail'))
+home_blueprint.add_url_rule('/login/', view_func=account.LoginView.as_view(name='login'))
+home_blueprint.add_url_rule('/register/', view_func=account.RegisterView.as_view(name='register'))
